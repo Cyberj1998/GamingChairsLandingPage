c6f87cd518b71b1db62fbecc76480b64f28824e9
Cyberj1998 <159168306+Cyberj1998@users.noreply.github.com>
2025-08-06 10:27:27 -0400

    Add files via upload

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,7 +27,7 @@ const Hero = ({dark, setDark}) => {
     }
 
   return (
-    <div className='h-[90vh] w-full flex flex-col justify-start items-center'>
+    <div className='h-[90vh] w-full flex flex-col justify-start items-center mb-[85px]'>
         <motion.div
         variants={variants}
         initial="hidden"
@@ -52,14 +52,27 @@ const Hero = ({dark, setDark}) => {
         className='flex w-full justify-center'
         {...slideAnimation('left')}
         >
-            <p className={`${dark == true ? 'text-[#727272]' : 'text-[#57415f]'} text-center font-sans font-semibold md:w-[50%] w-[80%] md:text-[15px] text-[12px]`}>Tired of lorem ipsum data? Here is a simple but easy-to-use free <span className='bg-gradient-to-r from-[#4f03ac] to-[#9a5ae7] text-transparent bg-clip-text'>API</span> that you can use to develop your project with <span className='bg-gradient-to-r from-[#4f03ac] to-[#9a5ae7] text-transparent bg-clip-text'>Real-Data</span>. You will get images, names of the products, prices, and descriptions. <span className='bg-gradient-to-r from-[#4f03ac] to-[#9a5ae7] text-transparent bg-clip-text'>Right now</span>, we only have one endpoint, but we are working to add many more in the <span className='bg-gradient-to-r from-[#4f03ac] to-[#9a5ae7] text-transparent bg-clip-text'>Future</span>. Feel free to use our gaming chairs API, this page is an example of the amazing things that you can build{'\u{1F680}'}</p>
+            <p className={`${dark == true ? 'text-[#727272]' : 'text-[#57415f]'} text-center font-sans font-semibold md:w-[50%] w-[80%] md:text-[15px] text-[12px]`}>Tired of lorem ipsum data? Here is a simple but easy-to-use free <span className='bg-gradient-to-r from-[#4f03ac] to-[#9a5ae7] text-transparent bg-clip-text'>API</span> that you can use to develop your project with <span className='bg-gradient-to-r from-[#4f03ac] to-[#9a5ae7] text-transparent bg-clip-text'>Real-Data</span>. You will get images, names of the products, prices, and descriptions. <span className='bg-gradient-to-r from-[#4f03ac] to-[#9a5ae7] text-transparent bg-clip-text'>Right now</span>, we only have two endpoint, but we are working to add many more in the <span className='bg-gradient-to-r from-[#4f03ac] to-[#9a5ae7] text-transparent bg-clip-text'>Future</span>. Feel free to use our gaming chairs API, this page is an example of the amazing things that you can build{'\u{1F680}'}</p>
         </motion.div>
         <motion.div
-         className='flex flex-row justify-center items-center w-fit'
+         className='flex flex-col justify-center items-center w-fit m-2'
          {...headContentAnimation}
          >
-            <input className={`md:w-[400px] w-[300px] rounded-[5px] bg-transparent border-[1px] border-[#57415f] ${dark == true ? 'text-white' : ''}`} ref={inputRef} value="https://gamingchairsapi.onrender.com/gamingChairs" readOnly />
-            <button onClick={handleClick} className='w-[30px] h-[30px] border border-[#57415f] rounded-[5px] flex justify-center items-center'><img src={clipboardIcon} className='w-[20px] h-[20px]' /></button>
+            <p>call all the chairs</p>
+            <div className='flex flex-row'>
+              <input className={`md:w-[400px] w-[300px] rounded-[5px] bg-transparent border-[1px] border-[#57415f] ${dark == true ? 'text-white' : ''}`} ref={inputRef} value="https://gamingchairsapi.onrender.com/gamingChairs" readOnly />
+              <button onClick={handleClick} className='w-[30px] h-[30px] border border-[#57415f] rounded-[5px] flex justify-center items-center'><img src={clipboardIcon} className='w-[20px] h-[20px]' /></button>
+            </div>
+        </motion.div>
+        <motion.div
+         className='flex flex-col justify-center items-center w-fit m-2'
+         {...headContentAnimation}
+         >
+            <p>call a chair by it's id</p>
+            <div className='flex flex-row'>
+              <input className={`md:w-[400px] w-[300px] rounded-[5px] bg-transparent border-[1px] border-[#57415f] ${dark == true ? 'text-white' : ''}`} ref={inputRef} value="https://gamingchairsapi.onrender.com/gamingChairs/1" readOnly />
+              <button onClick={handleClick} className='w-[30px] h-[30px] border border-[#57415f] rounded-[5px] flex justify-center items-center'><img src={clipboardIcon} className='w-[20px] h-[20px]' /></button>
+            </div>
         </motion.div>
     </div>
   )
